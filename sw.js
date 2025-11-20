76e70446178c906c6fb81d23179c7a159a213d6e
waqasmayur <faizakhano315@gmail.com>
2025-11-21 02:00:03 +0500

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE_NAME='future-mirror-v1';
-const ASSETS=['/','/index.html','/style.css','/app.js','/manifest.json'];
-self.addEventListener('install',evt=>{evt.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)));self.skipWaiting();});
-self.addEventListener('activate',evt=>{evt.waitUntil(self.clients.claim());});
-self.addEventListener('fetch',evt=>{evt.respondWith(caches.match(evt.request).then(res=>res||fetch(evt.request)));});
